0c99ae56e37ed97684fc3e31022c5fb2784d4f34
Shamel Lakin <christlife4u@gmail.com>
2019-09-24 23:59:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,3 +1,4 @@
+
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
@@ -24,25 +25,24 @@ end
 
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
-holiday_hash[:winter][:christmas][:new_years] = "balloons"
+  if season == :winter
+  holiday_hash.each do |season, festivities|
+    festivities << supply
+  end
+ end
 end
-puts holiday_hash
+
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
- holiday_hash[:spring] = "tablecloth"
+  if season == :spring
+  holiday_hash.each do |season, festivities|
+    festivities << supply
+  end
+ end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_hash[:fall][:halloween] = "candy","pumpkins"
-  # code here
-  # remember to return the updated hash
-  puts holiday_hash
-
+  holiday_hash[season][holiday_name] = supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
@@ -52,15 +52,8 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
-  holiday_hash.each do |season, holiday_name_hash|
-    print holiday_hash
+  holiday_hash.each do |season, festivities|
+    festivities << supply
   end
 end
 
@@ -70,7 +63,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  
 end
 
 
