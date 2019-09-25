8f648981ff010b0b4c8d69b3cbd2e6f4d5b2cf1d
Shamel Lakin <christlife4u@gmail.com>
2019-09-25 00:01:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,12 +52,14 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  holiday_hash.each do |season, festivities|
-    festivities << supply
+ holiday_supplies.each do |season, holidays|
+  holiday_hash.each do |season, holidays|
+    puts "#{season.capitalize}:"
+    holidays.each do |holiday, supplies|
+      puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
+    end
+   end
   end
-end
-
-
 end
 
 def all_holidays_with_bbq(holiday_hash)
